b979797bb477d3b0c33e2dd8919bb3e0a8fae318
Sebastian Barcelona <sbarcelona@gmail.com>
2022-06-15 23:37:09 +0000

    added solution for problems

--- src/app.py
+++ src/app.py
@@ -1,1 +1,46 @@
 # your app code here
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import sqlite3
+
+url = 'https://www.macrotrends.net/stocks/charts/TSLA/tesla/revenue'
+data = requests.get(url).text
+bs = BeautifulSoup(data, 'html.parser')
+tables = bs.find_all('table')
+
+pd_tesla_revenue = pd.DataFrame(columns=["Date", "Revenue"])
+features = pd.DataFrame()
+
+for index, table in enumerate(tables):
+    if ("Tesla Quarterly Revenue" in str(table)):
+        for row in table.tbody.find_all("tr"):
+            col = row.find_all("td")
+            # if col have len and revenue
+            if len(col) and col[1].text != '':
+                Date = col[0].text
+                Revenue = col[1].text.replace("$", "").replace(",", "")
+                aux_df = pd.DataFrame([{"Date":Date, "Revenue":Revenue}])
+                pd.concat([aux_df, features], axis=0, ignore_index=True)
+                
+pd_tesla_revenue = pd.concat([features, pd_tesla_revenue], axis=1)
+# List of records
+records_to_store = list(pd_tesla_revenue.to_records(index=False))
+# Generate DB
+db = sqlite3.connect('Tesla.db')
+cursor = db.cursor()
+
+# Create Table
+create_table = '''CREATE TABLE IF NOT EXISTS revenue (Date, Revenue)'''
+cursor.execute(create_table)
+
+# Insert data
+cursor.executemany('INSERT INTO revenue VALUES (?,?)', records_to_store)
+db.commit()
+
+# Review data stored
+select = '''SELECT * FROM revenue'''
+data_stored = cursor.execute(select)
+for el in data_stored:
+    print(el)
+
